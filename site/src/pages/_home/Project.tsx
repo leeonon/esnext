adef6eb134f1222f3d752ca1d89ebcb9ff9c1ddc
leeonon <v5sollee@gmail.com>
2023-10-04 12:42:09 +0800

    feat: update project list load more

--- site/src/pages/_home/Project.tsx
+++ site/src/pages/_home/Project.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { Button } from "@nextui-org/react";
-import { useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 
 import ProjectBox, { ProjectSkeleton } from "~/components/ProjectBox";
 import { api } from "~/utils/api";
 
 export default function Project() {
+  const loadingRef = useRef<HTMLDivElement>(null);
   const query = api.project.query.useInfiniteQuery(
     {
       limit: 10,
@@ -20,33 +21,51 @@ export default function Project() {
   const { data, hasNextPage, fetchNextPage, isLoading } = query;
   const { pages = [] } = data ?? {};
 
-  const onMore = () => {
-    if (hasNextPage) {
+  useEffect(() => {
+    if (!loadingRef.current) return;
+
+    const ob = new IntersectionObserver((entries) => {
+      if (!entries[0]?.isIntersecting) {
+        return;
+      }
+      if (isLoading) {
+        return;
+      }
+      console.log("Loading more projects...");
       fetchNextPage().catch((err) => console.error(err));
-    }
-  };
+    });
+    ob.observe(loadingRef.current);
+  }, [fetchNextPage, isLoading]);
 
   const currentList = useMemo(() => {
     return pages.flatMap((page) => page.list);
   }, [pages]);
 
   return (
-    <div className="flex h-fit flex-grow flex-wrap gap-4">
-      {!isLoading &&
-        currentList.map((project) => (
-          <ProjectBox
-            className="w-full flex-auto sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4"
-            key={project.id}
-            item={project}
-          />
-        ))}
-      {isLoading
-        ? Array.from({ length: 10 }).map((_, index) => (
-            <ProjectSkeleton key={index} />
-          ))
-        : null}
-
-      <Button onClick={onMore}>加载更多</Button>
+    <div>
+      <div className="flex h-fit flex-grow flex-wrap gap-4">
+        {!isLoading &&
+          currentList.map((project) => (
+            <ProjectBox
+              className="w-full flex-auto sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4"
+              key={project.id}
+              item={project}
+            />
+          ))}
+      </div>
+      {hasNextPage && (
+        <div
+          className="mt-4 flex h-fit flex-grow flex-wrap gap-4"
+          ref={loadingRef}
+        >
+          <ProjectSkeleton />
+          <ProjectSkeleton />
+          <ProjectSkeleton />
+          <ProjectSkeleton />
+          <ProjectSkeleton />
+          <ProjectSkeleton />
+        </div>
+      )}
     </div>
   );
 }
