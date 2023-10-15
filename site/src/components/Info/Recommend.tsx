af76185d9fb63e87dcd0a3abe7f5b83cb3989d30
leeonon <v5sollee@gmail.com>
2023-10-16 00:58:16 +0800

    update

--- site/src/components/Info/Recommend.tsx
+++ site/src/components/Info/Recommend.tsx
@@ -1,38 +1,44 @@
-import { Card, Image } from "@nextui-org/react";
+import { Image } from "@nextui-org/react";
 import NextImage from "next/image";
 
 const RecommendCard = () => {
   return (
-    <Card>
-      <div className="max-w-full">
-        <Image
-          as={NextImage}
-          isBlurred
-          width={70}
-          height={70}
-          radius="lg"
-          className="projectBoxImage"
-          src="https://lee-oss-1300118632.cos.ap-nanjing.myqcloud.com/obsidian/202310091757604.jpg"
-          alt=""
-        />
-        <p>Static</p>
-        <p className="overflow-ellipsis">
-          Boost.space tech stack Were aware of 11 technologies that tech stack
-          Ne Recent launches cloud
-        </p>
-      </div>
-    </Card>
+    <div className="grid max-w-full cursor-pointer grid-cols-[60px_1fr]">
+      <Image
+        as={NextImage}
+        isBlurred
+        width={60}
+        height={60}
+        radius="lg"
+        classNames={{
+          wrapper: "col-span-1 row-span-2",
+        }}
+        className="projectBoxImage"
+        src="https://lee-oss-1300118632.cos.ap-nanjing.myqcloud.com/obsidian/202310091757604.jpg"
+        alt=""
+      />
+      <p className="col-span-1 text-sm">Static</p>
+      <p className="col-span-1 line-clamp-2 max-w-full text-xs text-default-400">
+        Boost.space tech stack Were aware of 11 technologies that tech stack Ne
+        Recent launches cloud
+      </p>
+    </div>
   );
 };
 
 const Recommend = () => {
   return (
-    <div className="max-w-full flex-col gap-3">
-      <RecommendCard />
-      <RecommendCard />
-      <RecommendCard />
-      <RecommendCard />
-      <RecommendCard />
+    <div className="border-none border-opacity-0 bg-transparent">
+      <div>
+        <p className="text-tiny font-bold uppercase">Recommend</p>
+      </div>
+      <div className="mt-4 flex max-w-full flex-col gap-4">
+        <RecommendCard />
+        <RecommendCard />
+        <RecommendCard />
+        <RecommendCard />
+        <RecommendCard />
+      </div>
     </div>
   );
 };
