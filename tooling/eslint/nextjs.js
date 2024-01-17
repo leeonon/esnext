e1e5df5a8206c318c710f7af2607012871238558
leeonon <v5sollee@gmail.com>
2024-01-18 00:47:59 +0800

    chore: exnext tooling change to otter-hacker

--- tooling/eslint/nextjs.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-const config = {
-  extends: ['next/core-web-vitals'],
-  rules: {
-    '@next/next/no-html-link-for-pages': 'off',
-  },
-};
-
-module.exports = config;
